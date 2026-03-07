89afc4e36e97cf310ad9a6bf2c4e43ba3f789526
Daniele Cebin <danielecebin@gmail.com>
2026-03-07 14:41:11 -0300

    chore: add Jest configuration and setup files for testing

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/app/$1',
+    '^@/src/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default createJestConfig(config)
